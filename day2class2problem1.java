474698de2349b36855235afa9585a7f3a6879bc9
luisrialva <74136642+luisrialva@users.noreply.github.com>
2021-10-28 15:45:37 -0400

    Delete day2class2problem1.java

--- day2class2problem1.java
+++
@@ -1,9 +0,0 @@
-package week11;
-
-import java.util.Scanner;
-
-public class day2class2problem1 {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Enter a credit card number as a string");
-	
-}
